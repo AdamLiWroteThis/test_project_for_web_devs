597fa335cff054eac2198d0dda902f736a508034
AmberGlasses <775837740@qq.com>
2020-11-21 15:23:46 +0800

    重构views/Index.vue代码

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -18,57 +18,72 @@ export default {
     }
   },
   mounted () {
-    this.init()
+    this.setTimer()
   },
   destroyed () {
     // 销毁timer
     clearInterval(this.timer)
   },
   methods: {
-    /*
-      初始化定时任务
-     */
-    init: function () {
-      const timer = setInterval(() => {
-        const beforeGetTimeStamp = (new Date()).getTime()
-        getUrlResult().then(res => {
-          this.resData = res.data
-          const afterGetTimeStamp = (new Date()).getTime()
-          this.setLogs(res, beforeGetTimeStamp, afterGetTimeStamp - beforeGetTimeStamp)
-        }).catch((err) => {
-          // 留下空间处理报错信息，因为题目业务单一，所以未做单独处理与成功后记录成功的状态逻辑一致
-          this.resData = err.data
-          const afterGetTimeStamp = (new Date()).getTime()
-          this.setLogs(err, beforeGetTimeStamp, afterGetTimeStamp - beforeGetTimeStamp)
-        })
+    setTimer: function () {
+      const timer = setInterval(async () => {
+        const log = await this.getAPIResult()
+        this.setLogs(log)
       }, 5000)
 
       // 赋值timer，便于销毁
       this.timer = timer
     },
     /*
-      记录访问日志
+    return log
      */
-    setLogs: function (res, getRequstTimeStamp, durationTime) {
+    getAPIResult () {
+      return new Promise(resolve => {
+        // 记录发起请求时间
+        const beforeGetTimeStamp = (new Date()).getTime()
+        // 发起请求
+        getUrlResult().then(res => {
+          this.resData = res.data
+          const afterGetTimeStamp = (new Date()).getTime()
+          // 返回日志信息
+          resolve({
+            res: res,
+            getRequstTimeStamp: beforeGetTimeStamp,
+            durationTime: afterGetTimeStamp - beforeGetTimeStamp
+          })
+          // this.setLogs(res, beforeGetTimeStamp, afterGetTimeStamp - beforeGetTimeStamp)
+        }).catch((err) => {
+          // 留下空间处理报错信息，因为题目业务单一，所以未做单独处理与成功后记录成功的状态逻辑一致
+          this.resData = err.data
+          const afterGetTimeStamp = (new Date()).getTime()
+          // 返回错误日志信息
+          resolve({
+            res: err,
+            getRequstTimeStamp: beforeGetTimeStamp,
+            durationTime: afterGetTimeStamp - beforeGetTimeStamp
+          })
+          // this.setLogs(err, beforeGetTimeStamp, afterGetTimeStamp - beforeGetTimeStamp)
+        })
+      })
+    },
+    setLogs: function (log) {
       let logs = getLogs()
       if (!logs) {
         logs = []
       }
 
       const timeStamp = new Date()
-      timeStamp.setTime(getRequstTimeStamp)
+      timeStamp.setTime(log.getRequstTimeStamp)
       console.log(timeStamp.toUTCString())
-      // 初始化log
-      const log = {
+      const resultLog = {
         getRequstTimeStamp: timeStamp.toUTCString(),
-        status: res.status,
-        statusText: res.status === 200 ? '成功' : '失败',
-        durationTime: durationTime
+        status: log.res.status,
+        statusText: log.res.status === 200 ? '成功' : '失败',
+        durationTime: log.durationTime
       }
-      logs.push(log)
+      logs.push(resultLog)
       saveLogs(logs)
     }
   }
-
 }
 </script>
